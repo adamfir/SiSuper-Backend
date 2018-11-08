0dafce47d9a293a5bf9ade36d627758d88fe6511
Noer Herlambang <noer.herlambang@yahoo.com>
2018-11-08 08:38:09 +0700

    testing server 1

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var app = express();
 // connect to db
 // connect to database
 // db activated mongod.exe --dbpath "c:\mongodb\data"
+mongoose.connect('mongodb://127.0.0.1:27017');
 mongoose.connect('mongodb://localhost:27017/SiSuper2', {
     useNewUrlParser: true 
 })
