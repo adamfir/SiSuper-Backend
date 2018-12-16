d0630f24e25ecbdf745b0ad85b920754ab544403
Noer Herlambang <noer.herlambang@yahoo.com>
2018-12-16 16:53:46 +0700

    typograph

--- routes/products.js
+++ routes/products.js
@@ -55,7 +55,7 @@ router.get('/', (req, res, next) => {
 router.post('/addProduct', checkAuth, (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
-        bussiness_id: req.body.bussinessId,
+        business_id: req.body.bussinessId,
         name: req.body.productName,
         price: req.body.productPrice,
         unit: req.body.productUnit
